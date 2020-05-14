8031fcc88ba56a8ba5d7ef3d35f7ddad50733af9
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-14 14:09:31 -0400

    Update test_webpage_controller.py

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
+
 class test_webpage(http.Controller):
-	@http.route('/test/webpage',auth='user', website=True)
+	@http.route('/test/webpage', type='http', auth='user', website=True)
 	def index(self, **kw):
-		return http.request.render('test_webpage', {'name' :'slartybartfast'})
+		return http.request.render('chris_controller_testing.test_webpage', {'name' :'slartybartfast'})
