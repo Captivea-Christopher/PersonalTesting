af0aa6ee38924dfee4bdb79204b3067b4566b171
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-14 13:56:14 -0400

    Update __manifest__.py

--- chris_controller_testing/__manifest__.py
+++ chris_controller_testing/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "chris_controller_testing",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -27,9 +27,5 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    ]
 }
