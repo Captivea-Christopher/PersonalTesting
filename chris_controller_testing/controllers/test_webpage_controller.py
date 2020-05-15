74f531478da20891db2cc6928110ff6bb94af4c6
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-15 10:45:22 -0400

    asdf

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -7,4 +7,6 @@ class test_webpage(http.Controller):
 	def index(self, **kw):
 		current_user = http.request.session.uid
 		big_context = http.request.context
-		return http.request.render('chris_controller_testing.test_webpage', {'current_user' : current_user, 'big_context' : big_context})
+		long_user = "I KINDA WORK"
+		test_esc = 42
+		return http.request.render('chris_controller_testing.test_webpage', {'current_user' : current_user, 'big_context' : big_context, 'long_user' : long_user , 'test_esc' : test_esc})
