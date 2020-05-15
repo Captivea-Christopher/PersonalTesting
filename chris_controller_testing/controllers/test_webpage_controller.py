ee34bb5c343516335cbc4b2decb85d8ac6e3c6e2
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-15 14:02:13 -0400

    Test to see if it goes thru

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -5,11 +5,9 @@ from odoo.http import request
 class test_webpage(http.Controller):
 	@http.route('/test/webpage', type='http', auth='user', website=True)
 	def index(self, **kw):
-		list_of_projects_owned_by_customer = ["list","of","projects","owned"]
+		list_of_projects_owned_by_customer = ["Office Design", "Research & Development", "Test Project"]
 		user_email = "placeholder_email"
-		test_list = ["this","is","a","test","list"]
 		return http.request.render('chris_controller_testing.test_webpage', {
 			'list_of_projects_owned_by_customer': list_of_projects_owned_by_customer,
-			'user_email' : user_email,
-			'test_list' : test_list
+			'user_email' : user_email
 			})
