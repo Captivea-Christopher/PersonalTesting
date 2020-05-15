859b7ed4a530539893a7ccc100b4d8c4f0133413
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-15 13:48:14 -0400

    Update test_webpage_controller.py

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -5,8 +5,11 @@ from odoo.http import request
 class test_webpage(http.Controller):
 	@http.route('/test/webpage', type='http', auth='user', website=True)
 	def index(self, **kw):
+		list_of_projects_owned_by_customer = ["list","of","projects","owned"]
+		user_email = "placeholder_email"
+		test_list = ["this","is","a","test","list"]
 		return http.request.render('chris_controller_testing.test_webpage', {
-			'list_of_projects_owned_by_customer': list_of_projects_owned_by_customer
-			'user_email' : user_email
+			'list_of_projects_owned_by_customer': list_of_projects_owned_by_customer,
+			'user_email' : user_email,
 			'test_list' : test_list
 			})
