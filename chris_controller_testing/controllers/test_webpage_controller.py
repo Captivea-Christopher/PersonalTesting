e5a0d186b459e25ca815c92e4d629609610af3c7
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-15 10:11:31 -0400

    Update test_webpage_controller.py

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -5,7 +5,5 @@ from odoo import http
 class test_webpage(http.Controller):
 	@http.route('/test/webpage', type='http', auth='user', website=True)
 	def index(self, **kw):
-		context = self._context
-		current_uid = context.get('uid')
-		current_user = self.env['res.users'].browse(current_uid)
+		current_user = request.session.uid
 		return http.request.render('chris_controller_testing.test_webpage', {'customer_name' : current_user})
