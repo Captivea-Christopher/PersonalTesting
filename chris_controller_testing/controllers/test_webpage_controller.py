b485295807110450e39bc0a30f08c4b8561cad48
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-15 10:02:51 -0400

    Update test_webpage_controller.py

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -4,6 +4,6 @@ from odoo import http
 
 class test_webpage(http.Controller):
 	@http.route('/test/webpage', type='http', auth='user', website=True)
-	vars current_user = fields.Many2one('res.users','Current User', default=lambda self: self.env.user)
 	def index(self, **kw):
+		current_user = fields.Many2one('res.users','Current User', default=lambda self: self.env.user)
 		return http.request.render('chris_controller_testing.test_webpage', {'customer_name' : current_user})
