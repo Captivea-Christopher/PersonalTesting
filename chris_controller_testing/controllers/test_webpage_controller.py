095cee3b1c6c08b3672ed51d29c5feae18f61968
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-15 10:16:07 -0400

    asd

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -6,4 +6,4 @@ class test_webpage(http.Controller):
 	@http.route('/test/webpage', type='http', auth='user', website=True)
 	def index(self, **kw):
 		current_user = http.request.session.uid
-		return http.request.render('chris_controller_testing.test_webpage', {'customer_name' : current_user})
+		return http.request.render('chris_controller_testing.test_webpage', {'current_user' : current_user})
