876cf31db6e7dcd547ba546cf7078551e8db930f
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-15 11:07:13 -0400

    asd

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -5,13 +5,11 @@ from odoo.http import request
 class test_webpage(http.Controller):
 	@http.route('/test/webpage', type='http', auth='user', website=True)
 	def index(self, **kw):
-		current_user = http.request.session.uid
-		big_context = http.request.context
-		long_user = "I KINDA WORK"
-		test_esc = 42
+		list_of_projects_owned_by_customer = "placeholder"
+		user_email = "second_placeholder"
+		test_list = ["slarty","bart","fast"]
 		return http.request.render('chris_controller_testing.test_webpage', {
-			'current_user' : current_user,
-			'big_context' : big_context,
-			'long_user' : long_user ,
-			'test_esc' : test_esc
+			'list_of_projects_owned_by_customer' : list_of_projects_owned_by_customer
+			'user_email' : user_email
+			'test_list' : test_list
 			})
