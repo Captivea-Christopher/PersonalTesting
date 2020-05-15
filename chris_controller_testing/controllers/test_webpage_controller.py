de4781d1541fcda8f3ab335cfa58e3064efdce8e
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-15 09:53:54 -0400

    adsf

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -4,5 +4,6 @@ from odoo import http
 
 class test_webpage(http.Controller):
 	@http.route('/test/webpage', type='http', auth='user', website=True)
+	customer_name = 
 	def index(self, **kw):
-		return http.request.render('chris_controller_testing.test_webpage', {'customer_name' :'slartybartfast'})
+		return http.request.render('chris_controller_testing.test_webpage', {'customer_name' : "helaeld"})
