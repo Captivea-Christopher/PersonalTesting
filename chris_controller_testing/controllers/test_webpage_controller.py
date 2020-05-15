099b630c940e20ce7300a41597eb09f4195cf669
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-15 10:17:37 -0400

    Update test_webpage_controller.py

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -6,4 +6,5 @@ class test_webpage(http.Controller):
 	@http.route('/test/webpage', type='http', auth='user', website=True)
 	def index(self, **kw):
 		current_user = http.request.session.uid
-		return http.request.render('chris_controller_testing.test_webpage', {'current_user' : current_user})
+		big_context = http.request.context
+		return http.request.render('chris_controller_testing.test_webpage', {'current_user' : current_user, 'big_context' : big_context})
