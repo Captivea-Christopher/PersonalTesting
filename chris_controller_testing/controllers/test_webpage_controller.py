ea63b864d6f671f073f2cdd354ce92f8d67a9aa2
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-18 10:40:21 -0400

    Update test_webpage_controller.py

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -5,7 +5,7 @@ from odoo.http import request
 class test_webpage(http.Controller):
 	@http.route('/test/webpage', type='http', auth='user', website=True)
 	def index(self, **kw):
-		partner_id = request.user.partner_id
+		partner_id = request.env.user.partner_id
 		list_of_projects_owned_by_customer = request.env['project.project'].sudo().search([
 			('partner_id', '=', partner_id)
 		])
