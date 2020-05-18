9946d836f01103bd025fddd11b5d026830c1b505
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-18 09:27:43 -0400

    Update test_webpage_controller.py

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -9,7 +9,7 @@ class test_webpage(http.Controller):
 		list_of_projects_owned_by_customer = request.env['project.project'].sudo().search([
 			('partner_id', '=', user_uid)
 		])
-		test = request.env.self
+		test = request.env
 		customer_email = "placeholder_email"
 		return http.request.render('chris_controller_testing.test_webpage', {
 			'list_of_projects_owned_by_customer': list_of_projects_owned_by_customer,
