0286bee6d6b2f26dc104f6c9c9308bbbbc28ac39
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-18 15:15:03 -0400

    Added cap_portal_task_submission

---
+++ cap_portal_task_submission/controllers/portal_task_submission.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import http, models, fields
+from odoo.http import request
+
+class portal_task_submission(http.Controller):
+	@http.route('/test/webpage', type='http', auth='user', website=True)
+	def index(self, **kw):
+		partner_id = request.env.user.partner_id.id
+		list_of_projects_owned_by_customer = request.env['project.project'].sudo().search([
+			('partner_id', '=', partner_id)
+		])
+		test = list_of_projects_owned_by_customer
+		customer_email = "placeholder_email"
+		return http.request.render('chris_controller_testing.test_webpage', {
+			'list_of_projects_owned_by_customer': list_of_projects_owned_by_customer,
+			'customer_email' : customer_email,
+			'test' : test
+			})
