aad445e594ed727011ed26c7aa20ca427894f34b
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-18 10:00:05 -0400

    asfd

--- chris_controller_testing/controllers/test_webpage_controller.py
+++ chris_controller_testing/controllers/test_webpage_controller.py
@@ -6,10 +6,10 @@ class test_webpage(http.Controller):
 	@http.route('/test/webpage', type='http', auth='user', website=True)
 	def index(self, **kw):
 		user_uid = request.uid
-		list_of_projects_owned_by_customer = request.env['project.project'].sudo().search([
+		list_of_projects_owned_by_customer = self.env['project.project'].sudo().search([
 			('partner_id', '=', user_uid)
 		])
-		test = request.env
+		test = list_of_projects_owned_by_customer
 		customer_email = "placeholder_email"
 		return http.request.render('chris_controller_testing.test_webpage', {
 			'list_of_projects_owned_by_customer': list_of_projects_owned_by_customer,
